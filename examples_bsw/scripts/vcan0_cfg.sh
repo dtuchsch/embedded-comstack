a6a0e8ab17503a7159100e9f0ea171f77d519693
dtuchsch <daniel.tuchscherer@gmail.com>
2016-12-18 16:22:59 +0100

    Example adapted. Script for vcan0 setup.

---
+++ examples_bsw/scripts/vcan0_cfg.sh
@@ -0,0 +1,9 @@
+# Adds a virtual CAN device "vcan0" to the system, sets the MTU to activate CAN # FD support.
+
+sudo modprobe vcan
+# Add virtual can device
+sudo ip link add dev vcan0 type vcan
+# set mtu for CAN FD
+sudo ip link set vcan0 mtu 72
+# run virtual can device
+sudo ifconfig vcan0 up
