0333343e809eaf67e4095873e4198309cb2c5282
dtuchsch <daniel.tuchscherer@gmail.com>
2017-03-13 11:34:18 +0100

    tidy up

--- bsw/src/communication/Endianess.h
+++ bsw/src/communication/Endianess.h
@@ -38,46 +38,44 @@
  */
 
 #ifndef ENDIANESS_H_
-# define ENDIANESS_H_
+#define ENDIANESS_H_
 
 /*******************************************************************************
  * MODULES USED
- *******************************************************************************/
-# ifdef __unix__
-#  include <endian.h>
-# elif defined (_WIN32)
-#  define BYTE_ORDER (LITTLE_ENDIAN)
-# else
-#  error "Please #define BYTE_ORDER of your system architecture."
-# endif
+ ******************************************************************************/
+#ifdef __unix__
+#include <endian.h> //< under unix OS there is a BYTE_ORDER define.
+#elif defined(_WIN32)
+#define BYTE_ORDER (LITTLE_ENDIAN)
+#else
+#error "Please #define BYTE_ORDER for your system architecture."
+#endif
 
 /*******************************************************************************
  * DEFINITIONS AND MACROS
- *******************************************************************************/
+ ******************************************************************************/
 
 /*******************************************************************************
  * TYPEDEFS, ENUMERATIONS, CLASSES
- *******************************************************************************/
+ ******************************************************************************/
 
 /*******************************************************************************
  * EXPORTED VARIABLES
- *******************************************************************************/
+ ******************************************************************************/
 
 /*******************************************************************************
  * EXPORTED FUNCTIONS
- *******************************************************************************/
+ ******************************************************************************/
 
 ////////////////////////////////////////////////////////////////////////////////
-template< typename T, size_t Sz >
-T swap_bytes(const T& val) noexcept;
+template < typename T, size_t Sz > T swap_bytes(const T& val) noexcept;
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
  * @brief template specialization to swap an unsigned byte.
  * @return this will only return the byte again.
  */
-template< >
-inline uint8 swap_bytes< uint8, 1 >(const uint8& val) noexcept
+template <> inline uint8 swap_bytes< uint8, 1 >(const uint8& val) noexcept
 {
     return val;
 }
@@ -87,11 +85,10 @@ inline uint8 swap_bytes< uint8, 1 >(const uint8& val) noexcept
  * @brief template specialization to swap an unsgined word.
  * @return an unsigned word with swapped bytes.
  */
-template< >
-inline uint16 swap_bytes< uint16, 2 >(const uint16& val) noexcept
+template <> inline uint16 swap_bytes< uint16, 2 >(const uint16& val) noexcept
 {
     uint16 temp = 0U;
-    temp  = ((val >> 8U) & 0x00FFU);
+    temp = ((val >> 8U) & 0x00FFU);
     temp |= ((val << 8U) & 0xFFFFU);
     return temp;
 }
@@ -101,29 +98,28 @@ inline uint16 swap_bytes< uint16, 2 >(const uint16& val) noexcept
  * @brief template specialization to swap an unsgined word.
  * @return a signed word with swapped bytes.
  */
-template< >
-inline sint16 swap_bytes< sint16, 2 >(const sint16& val) noexcept
+template <> inline sint16 swap_bytes< sint16, 2 >(const sint16& val) noexcept
 {
     sint16 temp = 0;
-    temp  = ((val >> 8) & 0x00FF);
+    temp = ((val >> 8) & 0x00FF);
     temp |= ((val << 8) & 0xFFFF);
     return temp;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * @brief Template specialization for swapping 4 bytes of an unsgined double word.
+ * @brief Template specialization for swapping 4 bytes of an unsgined double
+ * word.
  * @param[in] val the value to byte-swap
  * @return a byte-swapped unsigned double word
  */
-template< >
-inline uint32 swap_bytes< uint32, 4 >(const uint32& val) noexcept
+template <> inline uint32 swap_bytes< uint32, 4 >(const uint32& val) noexcept
 {
-    uint32 temp = 0UL;
-    temp  = ((val >> 24U) & 0x000000FFUL); // byte 3 to 0
+    uint32 temp{0UL};
+    temp = ((val >> 24U) & 0x000000FFUL);  // byte 3 to 0
     temp |= ((val << 24U) & 0xFF000000UL); // byte 0 to 3
-    temp |= ((val >> 8U)  & 0x0000FF00UL); // byte 2 to 1
-    temp |= ((val << 8U)  & 0x00FF0000UL); // byte 1 to 2
+    temp |= ((val >> 8U) & 0x0000FF00UL);  // byte 2 to 1
+    temp |= ((val << 8U) & 0x00FF0000UL);  // byte 1 to 2
     return temp;
 }
 
@@ -133,14 +129,13 @@ inline uint32 swap_bytes< uint32, 4 >(const uint32& val) noexcept
  * @param[in] val the value to byte-swap
  * @return a byte-swapped unsigned double word
  */
-template< >
-inline sint32 swap_bytes< sint32, 4 >(const sint32& val) noexcept
+template <> inline sint32 swap_bytes< sint32, 4 >(const sint32& val) noexcept
 {
     sint32 temp = 0L;
-    temp  = ((val >> 24) & 0x000000FFL); // byte 3 to 0
+    temp = ((val >> 24) & 0x000000FFL);  // byte 3 to 0
     temp |= ((val << 24) & 0xFF000000L); // byte 0 to 3
-    temp |= ((val >> 8)  & 0x0000FF00L); // byte 2 to 1
-    temp |= ((val << 8)  & 0x00FF0000L); // byte 1 to 2
+    temp |= ((val >> 8) & 0x0000FF00L);  // byte 2 to 1
+    temp |= ((val << 8) & 0x00FF0000L);  // byte 1 to 2
     return temp;
 }
 
@@ -150,18 +145,17 @@ inline sint32 swap_bytes< sint32, 4 >(const sint32& val) noexcept
  * @param[in] val to swap the bytes
  * @return the swapped value
  */
-template< >
-inline uint64 swap_bytes< uint64, 8 >(const uint64& val) noexcept
+template <> inline uint64 swap_bytes< uint64, 8 >(const uint64& val) noexcept
 {
     uint64 temp = 0ULL;
-    temp  = ((val >> 56U) & 0x00000000000000FFULL); // byte 7 to 0
+    temp = ((val >> 56U) & 0x00000000000000FFULL);  // byte 7 to 0
     temp |= ((val << 56U) & 0xFF00000000000000ULL); // byte 0 to 7
     temp |= ((val >> 40U) & 0x000000000000FF00ULL); // byte 6 to 1
     temp |= ((val << 40U) & 0x00FF000000000000ULL); // byte 1 to 6
     temp |= ((val >> 24U) & 0x0000000000FF0000ULL); // byte 5 to 2
     temp |= ((val << 24U) & 0x0000FF0000000000ULL); // byte 2 to 5
-    temp |= ((val >> 8U)  & 0x00000000FF000000ULL); // byte 4 to 3
-    temp |= ((val << 8U)  & 0x000000FF00000000ULL); // byte 3 to 4
+    temp |= ((val >> 8U) & 0x00000000FF000000ULL);  // byte 4 to 3
+    temp |= ((val << 8U) & 0x000000FF00000000ULL);  // byte 3 to 4
     return temp;
 }
 
@@ -171,26 +165,25 @@ inline uint64 swap_bytes< uint64, 8 >(const uint64& val) noexcept
  * @param[in] val to swap the bytes
  * @return the swapped value
  */
-template< >
-inline sint64 swap_bytes< sint64, 8 >(const sint64& val) noexcept
+template <> inline sint64 swap_bytes< sint64, 8 >(const sint64& val) noexcept
 {
     sint64 temp = 0LL;
-    temp  = ((val >> 56U) & 0x00000000000000FFULL); // byte 7 to 0
+    temp = ((val >> 56U) & 0x00000000000000FFULL);  // byte 7 to 0
     temp |= ((val << 56U) & 0xFF00000000000000ULL); // byte 0 to 7
     temp |= ((val >> 40U) & 0x000000000000FF00ULL); // byte 6 to 1
     temp |= ((val << 40U) & 0x00FF000000000000ULL); // byte 1 to 6
     temp |= ((val >> 24U) & 0x0000000000FF0000ULL); // byte 5 to 2
     temp |= ((val << 24U) & 0x0000FF0000000000ULL); // byte 2 to 5
-    temp |= ((val >> 8U)  & 0x00000000FF000000ULL); // byte 4 to 3
-    temp |= ((val << 8U)  & 0x000000FF00000000ULL); // byte 3 to 4
+    temp |= ((val >> 8U) & 0x00000000FF000000ULL);  // byte 4 to 3
+    temp |= ((val << 8U) & 0x000000FF00000000ULL);  // byte 3 to 4
     return temp;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-template< >
+template <>
 inline float32 swap_bytes< float32, 4 >(const float32& fval) noexcept
 {
-    float32 float_swapped = 0.0F;
+    float32 float_swapped{0.0F};
     float32 temp = fval;
     uint8* float_to_convert = reinterpret_cast< uint8* >(&temp);
     uint8* to_convert = reinterpret_cast< uint8* >(&float_swapped);
@@ -202,10 +195,10 @@ inline float32 swap_bytes< float32, 4 >(const float32& fval) noexcept
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-template< >
+template <>
 inline float64 swap_bytes< float64, 8 >(const float64& fval) noexcept
 {
-    float64 float_swapped = 0.0F;
+    float64 float_swapped{0.0};
     float64 temp = fval;
     uint8* float_to_convert = reinterpret_cast< uint8* >(&temp);
     uint8* to_convert = reinterpret_cast< uint8* >(&float_swapped);
@@ -222,19 +215,18 @@ inline float64 swap_bytes< float64, 8 >(const float64& fval) noexcept
 
 ////////////////////////////////////////////////////////////////////////////////
 /**
- * @brief Convert a value of type T from host-byte-order into network-byte-order
- * which is big endian. A byte swap is only necessary if the host is little
- * endian.
+ * @brief Convert a value of type T from host-byte-order into
+ * network-byte-order, which is big endian. A byte swap is only necessary if the
+ * host is little endian.
  * @tparam T is the type of the value to convert.
  * @param[in] value to convert.
  * @return the network-byte-order value.
  */
-template< typename T >
-inline T to_network(const T& value) noexcept
+template < typename T > inline T to_network(const T& value) noexcept
 {
     T convert = value;
 
-#if ( BYTE_ORDER == LITTLE_ENDIAN )
+#if (BYTE_ORDER == LITTLE_ENDIAN)
     convert = swap_bytes< T, sizeof(T) >(value);
 #endif
 
@@ -249,15 +241,12 @@ inline T to_network(const T& value) noexcept
  * @param[in] value to convert
  * @return the host-byte-order value.
  */
-template< typename T >
-inline T from_network(const T& value) noexcept
+template < typename T > inline T from_network(const T& value) noexcept
 {
     T convert = value;
-
-#if ( BYTE_ORDER == LITTLE_ENDIAN )
+#if (BYTE_ORDER == LITTLE_ENDIAN)
     convert = swap_bytes< T, sizeof(T) >(value);
 #endif
-
     return convert;
 }
 
